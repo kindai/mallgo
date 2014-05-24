9ffb98c429bf6e0ce2398fdc934c5c67573af62e
Kin Lin <yuanlin1110@gmail.com>
2014-05-24 21:34:23 +0800

    test 2:2

--- src/main/java/com/mallgo/rest/UserRestBean.java
+++ src/main/java/com/mallgo/rest/UserRestBean.java
@@ -36,6 +36,7 @@ public class UserRestBean {
         RestUtil restUtil = new RestUtil();
 
         int kin1=0;
+        kin1=5;
 
         if(!restUtil.isValidToken(tokenCode, salt)){
             return Response.status(HttpServletResponse.SC_FORBIDDEN).entity("").build();
